39efccfc68d45aceed5caef9c83b8cda918a250d
Niteshpoudel <84695231+Niteshpoudel@users.noreply.github.com>
2021-09-06 16:57:25 +0545

    newcredit

--- rootPackage/Credit.py
+++ rootPackage/Credit.py
@@ -8,7 +8,17 @@ def open_credit():
     root.resizable(width=False, height=False)
     bg = PhotoImage(file="island2.png")
 
+    # Creating Canvas
+    my_canvas = Canvas(root, width=800, height=600, highlightthickness=0)
+    my_canvas.pack(fill="both", expand=False)
 
+    #Setting Canvas Image
+    my_canvas.create_image(0,0, image=bg, anchor="nw")
+
+    #Creating Label In Canvas
+    my_canvas.create_text(425, 200, text="""
+    All The Credit For Our Billion Dollar Game 
+        Goes To Our Core Team Members. 
     """, font=("arial",25,'bold'), fill="black")
 
     my_canvas.create_text(425, 350, text=""" 
