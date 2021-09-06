937922f221d91dfd24bb4a4e407342003b842b68
Niteshpoudel <84695231+Niteshpoudel@users.noreply.github.com>
2021-09-06 16:51:42 +0545

    Update Setting.py

--- rootPackage/Setting.py
+++ rootPackage/Setting.py
@@ -9,7 +9,18 @@ def open_setting():
     root.resizable(width=False, height=False)
     bg = PhotoImage(file="island3.png")
 
+    # Creating Canvas
+    my_canvas = Canvas(root, width=800, height=600, highlightthickness=0)
+    my_canvas.pack(fill="both", expand=False)
 
+    #Setting Canvas Image
+    my_canvas.create_image(0,0, image=bg, anchor="nw")
+    #n, ne, e, se, s, sw, w, nw, or center
+
+    def exit_setting():
+        root.destroy()
+
+    #Creating Control Button
     control_button = Button(root, text="CONTROL", bg="#59c2dc", bd=0, fg="white", font=("arial", 15, 'bold'), width=15, height=2, command=open_control)
     control_window = my_canvas.create_window(310, 150, anchor="nw", window=control_button)
 
